8baf3735105b59e1a1bc4d2d64da4d2c0e80f8be
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-22 03:38:14 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt () {
+  $("p").click(function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt () {
+    $("img").addClass("tasty").load(function() {
+  });
+}
+
+function pressIt () {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert("G was pressed.");
+    }
+  });
+}
+
+function submitIt () {
+  $("form").submit(function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+  return getIt();
+  return frameIt();
+  return pressIt();
+  return submitIt();
 });
