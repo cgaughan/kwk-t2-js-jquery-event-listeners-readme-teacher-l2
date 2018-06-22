55a58d6806da82a62ca0d58572aae0ab6fb13ee1
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-22 03:32:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,7 +2,7 @@
 function getIt () {
   $("p").click(function(){
     alert("Hey!");
-});
+  });
 }
 
 function frameIt () {
@@ -10,6 +10,17 @@ function frameIt () {
   });
 }
 
+function pressIt () {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+    }
+  });
+}
+
+function submitIt () {
+  
+}
 
 $(document).ready(function(){
 //  return getIt();
