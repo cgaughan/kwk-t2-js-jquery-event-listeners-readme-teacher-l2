e473ba6096956e94e0da5a4e023375b5dcbc1cd6
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-22 03:23:24 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,19 @@
 //define functions here
+function getIt () {
+  $("p").click(function(){
+    alert("Hey!");
+});
+}
+
+function frameIt () {
+  $(".tasty").load(function() {
+    $("img").addClass("tasty");
+  });
+}
+
 
 $(document).ready(function(){
-
+//  return getIt();
 // call functions here
 
 });
